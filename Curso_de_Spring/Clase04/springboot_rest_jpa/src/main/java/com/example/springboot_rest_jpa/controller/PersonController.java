df8a934cfccd73910e8be550310fed60849c04b2
biojuampa <104794253+biojuampa@users.noreply.github.com>
2024-08-25 01:54:19 -0300

    Clase 04
    Curso de Spring - OpenBootcamp

---
+++ Curso_de_Spring/Clase04/springboot_rest_jpa/src/main/java/com/example/springboot_rest_jpa/controller/PersonController.java
@@ -0,0 +1,107 @@
+package com.example.springboot_rest_jpa.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.springboot_rest_jpa.entity.Person;
+import com.example.springboot_rest_jpa.repository.PersonRepository;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+
+@RestController
+public class PersonController {
+
+    // Attributes
+    PersonRepository repository;
+
+    // Constructor (obtener el repository a través del constructor facilita el testing)
+    public PersonController(PersonRepository repository) {
+        this.repository = repository;
+    }
+
+    // Methods //
+    // Create
+    @PostMapping("/api/v1/users")
+    public ResponseEntity<Person> create(@RequestBody Person person) {        
+        try {
+             Person newPerson = repository.save(person);
+             URI uri = URI.create("http://localhost:8080/api/v1/users/" + newPerson.getId());
+             return ResponseEntity
+                     .created(uri)
+                     .body(newPerson)
+                     ;
+
+            // return ResponseEntity.ok(repository.save(person));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    
+    // Recovery
+    // 1. ALL users
+    @GetMapping("/api/v1/users")
+    public List<Person> findAll() {
+        return repository.findAll();
+    }
+    
+    // 2. One User by Id
+    @GetMapping("/api/v1/users/{id}")
+    public ResponseEntity<Person> findById(@PathVariable Long id) {
+        return repository
+            .findById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.badRequest().build())
+        ;
+
+        // Optional<Person> personOpt = repository.findById(id); 
+
+        // if (personOpt.isEmpty()) {
+        //     return ResponseEntity.badRequest().build();
+        // }
+        
+        // return ResponseEntity.ok(personOpt.get());
+    }
+    
+    // 3. Users by Name
+    @GetMapping("/api/v1/users/by-name/{name}")
+    public List<Person> findByName(@PathVariable String name) {
+        return repository
+                .findAll()
+                .stream()
+                .filter(p -> p.getName().equals(name))
+                .toList()
+                ;
+    }
+
+    // Update
+    @PutMapping("/api/v1/users/{id}")
+    public ResponseEntity<Person> update(@PathVariable Long id, @RequestBody Person person) {
+        if (repository.existsById(id)) {
+            repository.save(new Person(
+                    id,
+                    person.getName(),
+                    person.getEmail(),
+                    person.getRegistration(),
+                    person.getBlocked()
+            ));
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    // Delete
+    @DeleteMapping("/api/v1/users/{id}")
+    public ResponseEntity<Person> delete(@PathVariable Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.status(204).build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
---
+++ Curso_de_Spring/Clase04/springboot_rest_jpa/src/main/java/com/example/springboot_rest_jpa/entity/Person.java
@@ -0,0 +1,90 @@
+package com.example.springboot_rest_jpa.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="Users")
+public class Person {
+
+    // Attributes <==> Columns DDBB
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // Primary Key
+
+    private String name;
+    private String email;
+    private LocalDate registration;
+    private Boolean blocked;
+
+    // Constructors
+    public Person() {}
+
+    public Person(
+        Long id,
+        String name,
+        String email,
+        LocalDate registration,
+        Boolean blocked
+    ) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.registration = registration;
+        this.blocked = blocked;
+    }
+
+    // Methods
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getRegistration() {
+        return registration;
+    }
+    public void setRegistration(LocalDate registration) {
+        this.registration = registration;
+    }
+
+    public Boolean getBlocked() {
+        return blocked;
+    }
+    public void setBlocked(Boolean blocked) {
+        this.blocked = blocked;
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "Person [id=" + id
+            + ", name=" + name
+            + ", email=" + email
+            + ", registration="+ registration
+            + ", blocked=" + blocked
+            + "]"
+            ;
+    }
+    
+}
